b2a1d3dc717a686306985d4db139f1594a2b1383
CamCoder <camcam.95@icloud.com>
2020-08-14 19:32:01 -0500

    finished check and delete button

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 // Event Listeners
-
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 // Functions
 
@@ -21,7 +21,7 @@ function addTodo(event){
     //Create LI
     const newTodo = document.createElement('li');
     newTodo.classList.add('todo-item');
-    newTodo.innerText = 'Hey';
+    newTodo.innerText = todoInput.value;
 
     todoDiv.appendChild(newTodo);
 
@@ -35,10 +35,34 @@ function addTodo(event){
     //CHECK Trash button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('complete-btn');
+    trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton);
 
     //apend to list
-
     todoList.appendChild(todoDiv);
+
+    //clear todo input value
+    todoInput.value = "";
+}
+
+function deleteCheck(e){
+    const item = e.target;
+
+    //Delete todo
+    if(item.classList[0] === "trash-btn"){
+        const todo = item.parentElement;
+        //animation
+        todo.classList.add("fall");
+
+        todo.addEventListener('transitionend', function(){
+         todo.remove();           
+        })
+
+    }
+
+    //check mark
+    if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
